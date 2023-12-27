bf63f65c3de423d6a8a4c7d5764d67b8f69d1aa5
Daniel Penelva <d4n.andrade@gmail.com>
2023-12-27 03:19:43 -0300

    criando projeto spring boot 3

---
+++ src/test/java/com/api/customactuator/CustomActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.customactuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomActuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
